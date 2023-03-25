b38c4a5b59447f52cfebe82ffad4586b7ba66c3c
Developer <deidavel_62@outlook.com>
2023-03-24 22:52:15 -0400

    Verdadera-primera-version

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,4 +1,4 @@
-mport React from "react";
+import React from "react";
 import axios from "axios";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
--- proyecto-final-react/src/components/Footer.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Footer: React.FC = () => {
-  return <footer>JDSS 2023</footer>;
-};
-
-export default Footer;
